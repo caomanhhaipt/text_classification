5b6b2243a4fd9d5a93976f143f792c70391150ae
Cao Manh Hai <caomanhhaipt@gmail.com>
2018-11-01 14:28:10 +0700

    Demo with tf_idf

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ if __name__ == '__main__':
     contents_train, labels_train = data_loader.get_data_and_label()
 
     #Load dictionary
-    feature_extraction = feature_extraction.FeatureExtraction(None, 'dictionary.txt')
-    feature_extraction.data = contents_train
+    feature_extraction = feature_extraction.FeatureExtraction(contents_train, 'dictionary.txt')
     feature_extraction.load_dictionary()
 
+    #bag of words
     X_train = np.array(feature_extraction.bag_of_words(contents_train))
+
+    #tf_idf
+    # X_train = np.array(feature_extraction.tf_idf(contents_train))
     labels_train = np.array(labels_train)
 
     print(X_train.shape)
@@ -27,9 +30,18 @@ if __name__ == '__main__':
     #Test data
     data_loader.data_path = DIR_PATH + 'data/data_test/'
     contents_test, labels_test = data_loader.get_data_and_label()
+
+    #bag of words
     X_test = np.array(feature_extraction.bag_of_words(contents_test))
+
+    #tf_idf
+    #X_test = np.array(feature_extraction.tf_idf(contents_test))
     labels_test = np.array(labels_test)
 
+    print(X_test.shape)
+    print(labels_test.shape)
+
+    #Train model
     est = linear_svr.Classifier(features_train=X_train, features_test=X_test, labels_train=labels_train,
                      labels_test=labels_test)
     est.training()
