803e3b91743100a086eda50264d1ec3215ff06e4
Cao Manh Hai <caomanhhaipt@gmail.com>
2018-11-29 15:04:47 +0700

    shuffle data

--- utils/preprocessing_utils.py
+++ utils/preprocessing_utils.py
@@ -17,13 +17,22 @@ def split_data(X, y, test_size, shuffle=True):
 
     return X_train, y_train, X_test, y_test
 
+def shuffle_data(X, y):
+    ids_shuffle = np.array(range(X.shape[0]))
+    np.random.shuffle(ids_shuffle)
+
+    X_ = X[ids_shuffle]
+    y_ = y[ids_shuffle]
+
+    return X_, y_
+
 if __name__ == '__main__':
    a = np.array(range(1, 101))
    b = np.array(range(101, 201))
 
    X_train, y_train, X_test, y_test = split_data(a, b, 0.2)
 
-   print (X_train.shape)
-   print (y_train.shape)
-   print (X_test.shape)
-   print (y_test.shape)
+   print (X_train)
+   print (y_train)
+   print (X_test)
+   print (y_test)
